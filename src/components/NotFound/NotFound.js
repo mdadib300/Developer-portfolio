44180c340deee2cca1cfe26c29841b4770f7bc8c
Md. Adib Chowdhury <mdadib300@gmail.com>
2023-02-19 11:17:36 +0600

    'Error page' updated

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import notFound from '../../assets/icons/notFound.png';
 
 const NotFound = () => {
     return (
-        <div>
-            <h2 className='text-center text-3xl text-white my-60'>404: Page Not Found ☹</h2>
+        <div className='flex flex-col items-center mt-10'>
+            <img src={notFound} alt='Page not found | 404' className='w-60'></img>
+            <h2 className='text-center text-3xl my-10'>404: Page Not Found ☹</h2>
         </div>
     );
 };
