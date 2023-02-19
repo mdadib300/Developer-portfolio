2ff728c460560a55dbfda7cf823b4ffebbdd6501
Md. Adib Chowdhury <mdadib300@gmail.com>
2023-02-19 11:19:17 +0600

    'Portfolio' updated

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,20 +1,17 @@
 import React from 'react';
 import projects from '../../assets/icons/projectLogo.png';
 import ideaBoard from '../../assets/projects/idea-board.png';
-import project5 from '../../assets/projects/Jerins-parlour.png';
-import project6 from '../../assets/projects/Influencer-Point.png';
+import parlour from '../../assets/projects/Jerins-parlour.png';
+import influencer from '../../assets/projects/Influencer-Point.png';
 import { Link } from 'react-router-dom';
 
 const Portfolio = () => {
     return (
         <div className='m-3'>
-            
-
             <div className='flex items-center justify-center'>
-            <h1 className="text-4xl font-semibold text-center my-10 lg:my-20">Portfolio</h1>
-                <img src={projects}  className='w-[40px] ml-2'></img>
+                <h1 className="text-4xl font-semibold text-center my-10 lg:my-20">Portfolio</h1>
+                <img src={projects} alt='Projects' className='w-[40px] ml-2'></img>
             </div>
-
             <div className='w-3/4 mx-auto mb-7'>
                 <h2 className='text-3xl font-semibold mb-0 lg:mb-5'>1. Idea-Board</h2>
                 <div className='flex flex-col lg:flex-row-reverse items-center justify-around py-5 rounded-md'>
@@ -36,7 +33,7 @@ const Portfolio = () => {
             <div className='w-3/4 mx-auto mb-7'>
                 <h2 className='text-3xl font-semibold mb-0 lg:mb-5'>2. Influencer Point</h2>
                 <div className='flex flex-col lg:flex-row items-center justify-around py-5 rounded-md'>
-                    <img src={project6} alt='project demo' className='rounded-md w-full lg:w-[500px] mb-5 lg:mb-0 shadow-2xl'></img>
+                    <img src={parlour} alt='project demo' className='rounded-md w-full lg:w-[500px] mb-5 lg:mb-0 shadow-2xl'></img>
                     <div className='ml-0 lg:ml-10'>
                         <h2>Influencer point, an e-commerce landing page. Made with HTML, HTML5, CSS, CSS3, Tailwind CSS, DaisyUI. This webpage is fully responsive for mobile phone, tablet, laptop and desktop.</h2>
                         <div className='mt-5'>
@@ -56,7 +53,7 @@ const Portfolio = () => {
             <div className='w-3/4 mx-auto'>
                 <h2 className='text-3xl font-semibold mb-0 lg:mb-5'>3. Jerin's Parlour</h2>
                 <div className='flex flex-col lg:flex-row-reverse items-center justify-around py-5 rounded-md'>
-                    <img src={project5} alt='project demo' className='rounded-md w-full lg:w-[500px] mb-5 lg:mb-0 shadow-2xl'></img>
+                    <img src={influencer} alt='project demo' className='rounded-md w-full lg:w-[500px] mb-5 lg:mb-0 shadow-2xl'></img>
                     <div className='mr-0 lg:mr-10'>
                         <h2>Jerin's Parlour, an e-commerce landing page. Made with HTML, HTML5, CSS, CSS3, React, Bootstrap, React-Bootstrap. React Router and Bootstrap icons are also used. This webpage is fully responsive for mobile phone, tablet, laptop and desktop.</h2>
                         <div className='mt-5'>
@@ -74,7 +71,6 @@ const Portfolio = () => {
 
                 </div>
             </div>
-
             <div className='flex justify-center mt-10'>
                 <Link to='/projects' className='myWideBtn'>View More</Link>
             </div>
