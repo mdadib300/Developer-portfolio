496c1a9da358c025a6dab7e436b489ef314d70ae
Md. Adib Chowdhury <mdadib300@gmail.com>
2023-02-19 11:10:43 +0600

    'Email' updated

--- src/components/Email/Email.js
+++ src/components/Email/Email.js
@@ -3,13 +3,13 @@ import { Link } from 'react-router-dom';
 
 const Email = () => {
     return (
-        <div className='App my-32 mx-4 text-white'>
+        <div className='App my-32 mx-4'>
             <h2 className='text-2xl mb-5'>If you want to hire me then send an email following these details:</h2>
             <p className='text-xl'>Email Address: mdadib300@gmail.com</p>
             <p className='text-xl'>You email subject should be: 'Want to Hire You'</p>
             <div className='mt-10'>
                 <a className='myBtn my-5' href='https://mail.google.com/mail/u/0/#inbox?compose=new' target='blank'>Send Email</a>
-                <Link to='/' className='myBtn ml-5'>Go back to Homepage</Link>
+                <Link to='/' className='myBtn ml-5'>Jump back to Homepage</Link>
             </div>
         </div>
     );
